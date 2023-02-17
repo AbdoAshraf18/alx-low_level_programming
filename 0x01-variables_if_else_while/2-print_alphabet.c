155f0fc922c6f3826be965c01603ee7d19cbb4b1
AbdoAshraf18 <agaoa12345@gmail.com>
2023-02-17 12:53:11 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char P;
+
+	P = 'a';
+	while (P <= 'z')
+	{
+		putchar(P);
+	}
+	return (0);
+}
